f7c5faeb72cb97d33a380475216177066672f1e0
shutter-cp <37921589+shutter-cp@users.noreply.github.com>
2018-07-10 00:25:22 +0800

    Initial commit

---
+++ src/test/java/com/rwybbs/dao/TestDao.java
@@ -0,0 +1,35 @@
+/**
+ * chenPeng
+ * com.rwybbs.dao
+ * TestDao.java
+ * 创建人:chenpeng
+ * 时间：2018年7月9日-下午11:30:35 
+ * 2018陈鹏-版权所有
+ */
+package com.rwybbs.dao;
+
+import java.util.List;
+
+import com.rwybbs.bean.TestUser;
+
+/**
+ * 
+ * TestDao
+ * 创建人:chenPeng
+ * 时间：2018年7月9日-下午11:30:35 
+ * @version 1.0.0
+ * 
+ */
+public interface TestDao {
+	/**
+	 * 测试
+	 * 方法名：test
+	 * 创建人：chenPeng
+	 * 时间：2018年7月9日-下午11:33:10 
+	 * 手机:17673111810
+	 * @return List<TestUser>
+	 * @exception 
+	 * @since  1.0.0
+	 */
+	public List<TestUser> test();
+}
---
+++ src/test/java/com/rwybbs/web/TestWeb.java
@@ -0,0 +1,46 @@
+/**
+ * chenPeng
+ * com.rwybbs.web
+ * Test.java
+ * 创建人:chenpeng
+ * 时间：2018年7月9日-下午11:35:13 
+ * 2018陈鹏-版权所有
+ */
+package com.rwybbs.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.rwybbs.bean.TestUser;
+import com.rwybbs.dao.TestDao;
+
+/**
+ * 
+ * Test
+ * 创建人:chenPeng
+ * 时间：2018年7月9日-下午11:35:13 
+ * @version 1.0.0
+ * 
+ */
+@Controller
+public class TestWeb {
+	@Autowired
+	private TestDao testDao;
+	
+	@RequestMapping("/test")
+	public ModelAndView test(){
+		ModelAndView andView = new ModelAndView();
+		List<TestUser> users = testDao.test();
+		for (TestUser testUser : users) {
+			System.out.println(testUser.getId()+"      "+testUser.getUsername());
+		}
+		andView.addObject("users", users);
+		andView.setViewName("test");
+		System.out.println(1);
+		return andView;
+	}
+}
